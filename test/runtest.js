146ef20d7ac3317a91a6a9b809bc8bd524442185
michieljoris <michieljoris@gmail.com>
2012-09-27 15:47:24 +1000

    bugs, bugs

---
+++ test/runtest.js
@@ -0,0 +1,13 @@
+define (
+{
+  factory: "I never get to be produced, because this file is loaded with a .js extension"
+}
+
+);
+
+
+describe("In runtest.js", function() {
+      	   it("loading js files with .js extension runs them", function() {
+      		expect(1).toEqual(1);
+      	      });
+	 });
